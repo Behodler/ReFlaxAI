fc0060bd109776eb7ed0b4e34ae73c6a82fe8ff3
gititgoro <admin@behodler.io>
2025-04-17 03:17:30 +0200

    priceTilter natspec

--- src/priceTilting/PriceTilterTWAP.sol
+++ src/priceTilting/PriceTilterTWAP.sol
@@ -11,76 +11,94 @@ contract PriceTilter is Ownable {
     IERC20 public flaxToken;
     TWAPOracle public twapOracle;
 
-mapping(address => bool) public isPairRegistered;
+    mapping(address => bool) public isPairRegistered;
 
-event PairRegistered(address indexed tokenA, address indexed tokenB, address pair);
+    event PairRegistered(address indexed tokenA, address indexed tokenB, address pair);
 
-constructor(address _factory, address _router, address _flaxToken, address _twapOracle) Ownable(msg.sender) {
-    require(_factory != address(0), "Invalid factory address");
-    require(_router != address(0), "Invalid router address");
-    require(_flaxToken != address(0), "Invalid Flax token address");
-    require(_twapOracle != address(0), "Invalid TWAP oracle address");
-    factory = IUniswapV2Factory(_factory);
-    router = IUniswapV2Router02(_router);
-    flaxToken = IERC20(_flaxToken);
-    twapOracle = TWAPOracle(_twapOracle);
-}
-
-function registerPair(address tokenA, address tokenB) external onlyOwner {
-    require(tokenA != address(0) && tokenB != address(0), "Invalid token address");
-    require(tokenA != tokenB, "Tokens must be different");
-    address pair = factory.getPair(tokenA, tokenB);
-    require(pair != address(0), "Pair does not exist");
-    require(!isPairRegistered[pair], "Pair already registered");
-
-    isPairRegistered[pair] = true;
-    twapOracle.initializePair(pair);
-
-    emit PairRegistered(tokenA, tokenB, pair);
-}
-
-function getPrice(address tokenA, address tokenB) external returns (uint256) {
-    address pair = factory.getPair(tokenA, tokenB);
-    require(isPairRegistered[pair], "Pair not registered");
-
-    twapOracle.updateTWAP(pair);
-
-    address token0 = IUniswapV2Pair(pair).token0();
-    if (token0 == tokenA) {
-        return twapOracle.getPrice0(pair);
-    } else {
-        return twapOracle.getPrice1(pair);
+    constructor(address _factory, address _router, address _flaxToken, address _twapOracle) Ownable(msg.sender) {
+        require(_factory != address(0), "Invalid factory address");
+        require(_router != address(0), "Invalid router address");
+        require(_flaxToken != address(0), "Invalid Flax token address");
+        require(_twapOracle != address(0), "Invalid TWAP oracle address");
+        factory = IUniswapV2Factory(_factory);
+        router = IUniswapV2Router02(_router);
+        flaxToken = IERC20(_flaxToken);
+        twapOracle = TWAPOracle(_twapOracle);
     }
-}
 
-function addLiquidity(
-    address tokenA,
-    address tokenB,
-    uint256 amountA,
-    uint256 amountB
-) external {
-    address pair = factory.getPair(tokenA, tokenB);
-    require(isPairRegistered[pair], "Pair not registered");
 
-    IERC20(tokenA).transferFrom(msg.sender, address(this), amountA);
-    IERC20(tokenB).transferFrom(msg.sender, address(this), amountB);
+    /**
+     * @notice Registers a Uniswap V2 pair for TWAP calculations.
+     * @dev Only callable by the owner.
+     * @param tokenA The first token in the pair.
+     * @param tokenB The second token in the pair.
+     */
+    function registerPair(address tokenA, address tokenB) external onlyOwner {
+        require(tokenA != address(0) && tokenB != address(0), "Invalid token address");
+        require(tokenA != tokenB, "Tokens must be different");
+        address pair = factory.getPair(tokenA, tokenB);
+        require(pair != address(0), "Pair does not exist");
+        require(!isPairRegistered[pair], "Pair already registered");
 
-    IERC20(tokenA).approve(address(router), amountA);
-    IERC20(tokenB).approve(address(router), amountB);
+        isPairRegistered[pair] = true;
+        twapOracle.initializePair(pair);
 
-    router.addLiquidity(
-        tokenA,
-        tokenB,
-        amountA,
-        amountB,
-        0,
-        0,
-        address(this),
-        block.timestamp + 300
-    );
-}
+        emit PairRegistered(tokenA, tokenB, pair);
+    }
 
-function tiltPrice(address token, uint256 amount) external {}
+    /**
+     * @notice Retrieves the TWAP-based price of tokenA in terms of tokenB.
+     * @dev May update TWAP oracle state.
+     * @param tokenA The first token in the pair.
+     * @param tokenB The second token in the pair.
+     * @return The TWAP price as a uint256.
+     */
+    function getPrice(address tokenA, address tokenB) external returns (uint256) {
+        address pair = factory.getPair(tokenA, tokenB);
+        require(isPairRegistered[pair], "Pair not registered");
 
-}
+        twapOracle.updateTWAP(pair);
 
+        address token0 = IUniswapV2Pair(pair).token0();
+        if (token0 == tokenA) {
+            return twapOracle.getPrice0(pair);
+        } else {
+            return twapOracle.getPrice1(pair);
+        }
+    }
+
+    /**
+     * @notice Adds liquidity to a Uniswap V2 pair to influence price tilting.
+     * @param tokenA The first token in the pair.
+     * @param tokenB The second token in the pair.
+     * @param amountA The amount of tokenA to add.
+     * @param amountB The amount of tokenB to add.
+     */
+    function addLiquidity(
+        address tokenA,
+        address tokenB,
+        uint256 amountA,
+        uint256 amountB
+    ) external {
+        address pair = factory.getPair(tokenA, tokenB);
+        require(isPairRegistered[pair], "Pair not registered");
+
+        IERC20(tokenA).transferFrom(msg.sender, address(this), amountA);
+        IERC20(tokenB).transferFrom(msg.sender, address(this), amountB);
+
+        IERC20(tokenA).approve(address(router), amountA);
+        IERC20(tokenB).approve(address(router), amountB);
+
+        router.addLiquidity(
+            tokenA,
+            tokenB,
+            amountA,
+            amountB,
+            0,
+            0,
+            address(this),
+            block.timestamp + 300
+        );
+    }
+
+   }
